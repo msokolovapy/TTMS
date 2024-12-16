6f6b4bff1aa3be59e6cad61659337102cbdc7f78
Maria Sokolova <maria@MariaPC.auspep.local>
2024-12-16 16:52:06 +1100

    daily ranking update re-written and models_uuser re-written

--- daily_ranking_update.py
+++ daily_ranking_update.py
@@ -1,42 +1,32 @@
 # daily_ranking_update.py
 
+from app import app
 from datetime import datetime
 from extensions import db
-from models_match import Match
+from models_match import Match,retrieve_match_data
+from models_user import User
 from sqlalchemy import func
 import logging
 
 logging.basicConfig(filename='daily_ranking_update.log', level=logging.INFO, 
                     format='%(asctime)s %(levelname)s:%(message)s')
 
-def retrieve_match_data(match_date):
-    # today_date = datetime.now().strftime('%Y-%m-%d')
-    match_data_query = db.session.query(
-        Match.player_1_login_name,
-        Match.player_2_login_name,
-        Match.match_result
-    ).filter(func.date(Match.match_start_date_time) == match_date).all()
-    
-    logging.info(f"Retrieved match data for {match_date}")
-    return match_data_query
+def update_database_with_new_ranks(match):
+    winner_name, loser_name = match.determine_match_winner
+    new_winner_rank, new_loser_rank = match.update_player_ranking()
 
-def create_match_obj_list(match_date):
-    match_data_query_result = retrieve_match_data(match_date)
-    if match_data_query_result:
-        match_obj_list = [
-            Match(player_1_login_name=player_1_login_name, player_2_login_name=player_2_login_name, match_result=match_result)
-            for (player_1_login_name, player_2_login_name, match_result) in match_data_query_result
-        ]
-        logging.info(f"Created match object list with {len(match_obj_list)} matches.")
-    else:
-        match_obj_list = []  # Empty list if no matches found
-        logging.info("No games were played today. No match objects created.")
-    return match_obj_list
+    winner = User.query.filter_by(player_login_name=winner_name).first()
+    loser = User.query.filter_by(player_login_name=loser_name).first()
+    winner.player_rank = new_winner_rank
+    loser.player_rank = new_loser_rank
+
+    db.session.commit()
+
 
 def daily_ranking_update(match_obj_list):
     if match_obj_list:
         for match_obj in match_obj_list:
-            match_obj.update_ranking()
+            update_database_with_new_ranks(match_obj)
             logging.info(f"Updated player ranking for match between {match_obj.player_1_login_name} and {match_obj.player_2_login_name}.")
     else:
         logging.info("No match objects to update.")
@@ -44,12 +34,18 @@ def daily_ranking_update(match_obj_list):
 
 
 if __name__ == '__main__':
-    try:
-        match_obj_list = create_match_obj_list()
-        daily_ranking_update(match_obj_list)
-        logging.info("Daily player ranking update completed successfully.")
-    except Exception as e:
-        logging.error(f"Error occurred during player ranking update: {e}")
+    with app.app_context():
+        try:
+            #today_date = datetime.now().strftime('%Y-%m-%d')
+            today_date = '2024-12-04'
+            match_obj_list = retrieve_match_data(today_date)
+            for match in match_obj_list:
+                print(match.update_player_ranking())
+                
+            # daily_ranking_update(match_obj_list)
+            # logging.info("Daily player ranking update completed successfully.")
+        except Exception as e:
+            logging.error(f"Error occurred during player ranking update: {e}")
 
 
     
--- models_match.py
+++ models_match.py
@@ -1,7 +1,13 @@
 #models_match.py
 
 from extensions import db
+from elo_rating import Elo
+from models_user import User
+from sqlalchemy import func
+import logging
+import ast
 
+logger = logging.getLogger(__name__)
 
 class Match(db.Model):
     match_id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +27,38 @@ class Match(db.Model):
         if isinstance(other, Match):
             return sorted([self.player_1_login_name, self.player_2_login_name]) == sorted([other.player_1_login_name, other.player_2_login_name])
         return False
+
+
+
+    def convert_match_result_to_integer(self):
+        if self.match_result:
+            match_result = ast.literal_eval(self.match_result)
+            return tuple([tuple(map(int, score_duo)) for score_duo in match_result])
+        else:
+            logger.info("Match result doesn't exist. Impossible to convert to tuple")
+
+    def determine_match_winner(self):
+        match_result = self.convert_match_result_to_integer()
+        games_won_by_player_1 = sum(1 for score_duo in match_result if score_duo[0] > score_duo[1])
+        games_won_by_player_2 = sum(1 for score_duo in match_result if score_duo[1] > score_duo[0]) 
+     
+        if games_won_by_player_1 > games_won_by_player_2:  
+            return self.player_1_login_name, self.player_2_login_name
+        else:
+            return self.player_2_login_name, self.player_1_login_name                
+
+    def extract_current_player_rank(self):
+        winner_name, loser_name = self.determine_match_winner()
+        winner = User.query.filter_by(player_login_name=winner_name).first()
+        loser = User.query.filter_by(player_login_name=loser_name).first()
+        return winner.player_rank, loser.player_rank
+
+
+    def update_player_ranking(self):
+        winner_rank, loser_rank = self.extract_current_player_rank()
+        elo = Elo(k=32)
+        return elo.rate(winner_rank, loser_rank)
+
 
 
     def to_dict(self):
@@ -44,6 +82,12 @@ class Match(db.Model):
         )
         match.match_id = data.get('match_id', None)
         return match
+
+def retrieve_match_data(today_date):
+    match_obj_list = db.session.query(
+    Match).filter(func.date(Match.match_start_date_time) == today_date).all()
+    logger.info(f"Retrieved match data for {today_date}")
+    return match_obj_list
 
 if __name__ == '__main__':
     match1 = Match('mike','mike')
